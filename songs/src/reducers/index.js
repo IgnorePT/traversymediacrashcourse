69afb785f0367133a033b03e76e75d7db101a913
Nelson Fernandes <fa.nelson.fernandes@gmail.com>
2019-05-29 15:44:19 +0100

    Create Actions: Select Song
    Create reducer:´
    Songs Reducers
    - Return array of songs
    Select Song Reducer
    - Return song selected by user using action

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,17 @@
+const songsReducer = () => {
+	return [
+		{ title: "No scrubs", duration: "4:05" },
+		{ title: "Macarena", duration: "2:30" },
+		{ title: "all Star", duration: "3:15" },
+		{ title: "I want It", duration: "4:30" }
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+
+	if (action.type === "SONG_SELECTED"){
+		return action.payload;
+	}
+
+	return selectedSong;
+}
